52af38a73c25a23534ce4959102ad5287412dc4a
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2021-05-08 11:42:29 +0900

    feat: introduce controlled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React, { FC } from "react";
+import React, { FC, useState, ChangeEvent } from "react";
 import sketch from "sketch/circle";
 import Canvas from "canvas/canvas";
 
-const App: FC = () => (
-  <div>
-    <div>Hello world!</div>
-    <Canvas sketch={sketch} />
-    <textarea></textarea>
-  </div>
-);
+const App: FC = () => {
+  const [value, setValue] = useState("Hello world!");
+
+  const handleCange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <div>Hello world!</div>
+      <Canvas sketch={sketch} />
+      <textarea value={value} onChange={handleCange}></textarea>
+    </div>
+  );
+};
 
 export default App;
